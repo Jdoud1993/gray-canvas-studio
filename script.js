6efd6a8596825c89588d2d8e499e31de367c0619
Joshua Doud <bigjj3393@yahoo.com>
2022-09-23 22:51:44 -0700

    fixed slideshow one to show first image

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
+
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
+
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
