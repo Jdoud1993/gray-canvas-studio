3cfe846f79356f3dc040ea549d0df3e1e01d3581
Joshua Doud <bigjj3393@yahoo.com>
2022-09-23 23:31:29 -0700

    added fotter and configured top button

--- script.js
+++ script.js
@@ -26,3 +26,23 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+
+
+
+let mybutton = document.getElementsByClassName("Top");
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
